c1be100fd35056023d1634318dcb2413d288a506
Messias Fernandes <dev.messiasfer@gmail.com>
2025-07-04 20:41:13 -0300

    js

--- manipulação.js
+++ manipulação.js
@@ -1,45 +1,45 @@
-// const animais = document.querySelector('.animais')
-// const contato = document.querySelector('.contato')
-// const titulo = contato.querySelector('.titulo')
+const animais = document.querySelector('.animais')
+const contato = document.querySelector('.contato')
+const titulo = contato.querySelector('.titulo')
 
-// const mapa = document.querySelector('.mapa')
-// // contato.removeChild(titulo)
+const mapa = document.querySelector('.mapa')
+// contato.removeChild(titulo)
 
-// contato.replaceChild(mapa, titulo)
+contato.replaceChild(mapa, titulo)
 
-//           //CRIAR UMA NOVA DIV //
+          //CRIAR UMA NOVA DIV //
 
-// const novoh1 = document.createElement('h1')
+const novoh1 = document.createElement('h1')
 
-// novoh1.innerText = 'Novo titulo'
-// novoh1.classList.add('titulo')
+novoh1.innerText = 'Novo titulo'
+novoh1.classList.add('titulo')
 
-// mapa.appendChild(novoh1)
+mapa.appendChild(novoh1)
 
-//           clonar
+          clonar
 
-// const h1 = document.querySelector('h1')
-// const faq = document.querySelector('.faq')
+const h1 = document.querySelector('h1')
+const faq = document.querySelector('.faq')
 
-// const cloneH1 = h1.cloneNode(true)
+const cloneH1 = h1.cloneNode(true)
 
-// cloneH1.classList.add('azul')
-// faq.appendChild(cloneH1)
+cloneH1.classList.add('azul')
+faq.appendChild(cloneH1)
 
-// duplicar o menu e adicionar ao copy
-// const DupleMenu = document.querySelector('.menu')
-// const copy = document.querySelector('.copy')
-// const cloneMenu = DupleMenu.cloneNode(true)
-// copy.appendChild(cloneMenu)
+duplicar o menu e adicionar ao copy
+const DupleMenu = document.querySelector('.menu')
+const copy = document.querySelector('.copy')
+const cloneMenu = DupleMenu.cloneNode(true)
+copy.appendChild(cloneMenu)
 
-// Selecionar o primeiro dt do dl de faq
-// const primeiroDt = faq.querySelector('dt')
+Selecionar o primeiro dt do dl de faq
+const primeiroDt = faq.querySelector('dt')
 
-// Selecione o dd refente ao primeiro dt
-// const proximoDD = primeiroDt.nextElementSibling;
+Selecione o dd refente ao primeiro dt
+const proximoDD = primeiroDt.nextElementSibling;
 
-// subistitua  todo o conteudo html de faq pelo de animais
-// const faq = document.querySelector('.faq')
-// const animais = document.querySelector('.animais')
+subistitua  todo o conteudo html de faq pelo de animais
+const faq = document.querySelector('.faq')
+const animais = document.querySelector('.animais')
 
-// faq.innerHTML = animais.innerHTML
+faq.innerHTML = animais.innerHTML
