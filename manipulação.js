7fe004d4920e7506cb3284273efe8978bf96e5da
Messias Fernandes <dev.messiasfer@gmail.com>
2025-07-05 23:21:46 -0300

    js-2

--- manipulação.js
+++ manipulação.js
@@ -18,28 +18,28 @@ mapa.appendChild(novoh1)
 
           clonar
 
-const h1 = document.querySelector('h1')
-const faq = document.querySelector('.faq')
+// const h1 = document.querySelector('h1')
+// const faq = document.querySelector('.faq')
 
-const cloneH1 = h1.cloneNode(true)
+// const cloneH1 = h1.cloneNode(true)
 
-cloneH1.classList.add('azul')
-faq.appendChild(cloneH1)
+// cloneH1.classList.add('azul')
+// faq.appendChild(cloneH1)
 
-duplicar o menu e adicionar ao copy
-const DupleMenu = document.querySelector('.menu')
-const copy = document.querySelector('.copy')
-const cloneMenu = DupleMenu.cloneNode(true)
-copy.appendChild(cloneMenu)
+// duplicar o menu e adicionar ao copy
+// const DupleMenu = document.querySelector('.menu')
+// const copy = document.querySelector('.copy')
+// const cloneMenu = DupleMenu.cloneNode(true)
+// copy.appendChild(cloneMenu)
 
-Selecionar o primeiro dt do dl de faq
-const primeiroDt = faq.querySelector('dt')
+// Selecionar o primeiro dt do dl de faq
+// const primeiroDt = faq.querySelector('dt')
 
-Selecione o dd refente ao primeiro dt
-const proximoDD = primeiroDt.nextElementSibling;
+// Selecione o dd refente ao primeiro dt
+// const proximoDD = primeiroDt.nextElementSibling;
 
-subistitua  todo o conteudo html de faq pelo de animais
-const faq = document.querySelector('.faq')
-const animais = document.querySelector('.animais')
+// subistitua  todo o conteudo html de faq pelo de animais
+// const faq = document.querySelector('.faq')
+// const animais = document.querySelector('.animais')
 
-faq.innerHTML = animais.innerHTML
+// faq.innerHTML = animais.innerHTML
