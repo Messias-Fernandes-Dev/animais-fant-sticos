46c28608c03d628ae3bba741c884c81eb0c08c59
Messias Fernandes <dev.messiasfer@gmail.com>
2025-07-13 20:12:37 -0300

    js object

--- app.js
+++ app.js
@@ -22,69 +22,75 @@ function initTabNav() {
 
 initTabNav();
 
-function initAccordion() { 
-const accordionList = document.querySelectorAll('.js-accordion dt');
-const activeCLass = 'ativo';
-if(accordionList.length){
-accordionList[0].classList.add(activeCLass)
-accordionList[0].nextElementSibling.classList.add(activeCLass)
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeCLass = "ativo";
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeCLass);
+    accordionList[0].nextElementSibling.classList.add(activeCLass);
 
+    function activeAccordion() {
+      this.classList.toggle(activeCLass);
+      this.nextElementSibling.classList.toggle(activeCLass);
+    }
 
-function activeAccordion() {
-  this.classList.toggle(activeCLass)
- this.nextElementSibling.classList.toggle(activeCLass)
-}
-
-accordionList.forEach((item) => {
-  item.addEventListener('click', activeAccordion)
-})
-}
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
 }
 
 initAccordion();
 
-
 function initScrollSuave() {
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-function scrollToSection(event) {
- event.preventDefault();
- const href = event.currentTarget.getAttribute('href');
- const section = document.querySelector(href)
- 
- section.scrollIntoView({
-  behavior: 'smooth',
-  block: 'start',
- });
-}
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
 
-linksInternos.forEach((link) => {
-  link.addEventListener('click', scrollToSection)
-})
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
 }
 initScrollSuave();
 
 function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  const windowMetade = window.innerHeight * 0.6;
 
-const sections = document.querySelectorAll('.js-scroll')
-const windowMetade = window.innerHeight * 0.6;
+  if (sections.length) {
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) section.classList.add("ativo");
+        else section.classList.remove("ativo");
+      });
+    }
 
-if(sections.length){
-
-function animaScroll(){
-  sections.forEach((section) => {
-    const sectionTop = section.getBoundingClientRect().top;
-    const isSectionVisible = (sectionTop - windowMetade) < 0;
-    if(isSectionVisible)
-      section.classList.add('ativo')
-    else
-      section.classList.remove('ativo')
-  })
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
 }
 
-animaScroll();
-window.addEventListener('scroll', animaScroll)
-}
+initAnimacaoScroll();
+
+// const honda = new Carro("Honda", 3000);
+// const fiat = new Carro("Fiat", 4000);
+
+function Carro2(marca, precoInicial) {
+  const taxa = 1.2;
+  const precoFinal = precoInicial * taxa;
+  Carro2.marca = marca;
+  Carro2.preco = precoFinal;
 }
 
-initAnimacaoScroll()
+const mazda = new Carro2("Mazda", 5000);
